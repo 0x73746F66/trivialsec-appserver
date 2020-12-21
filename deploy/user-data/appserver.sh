facae7f5bc8209afba20038238f16462dde69b16
Christopher Langton <chris.langton@cmd.com.au>
2020-12-21 22:49:46 +1100

    dynamic var NUM_INSTANCES

--- deploy/user-data/appserver.sh
+++ deploy/user-data/appserver.sh
@@ -3,4 +3,4 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 runuser -l ec2-user -c 'time /home/ec2-user/.local/bin/uwsgi --listen $(cat /proc/sys/net/core/somaxconn) --daemonize -- /srv/app/app.ini'
 lsof -i :80 -i :5000
 /usr/sbin/nginx -t
-/usr/sbin/nginx || /usr/sbin/nginx -s reload
+/usr/sbin/nginx || /usr/sbin/nginx -s reopen
