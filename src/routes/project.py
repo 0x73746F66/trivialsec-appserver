17b499490b83f98649030d2f5e4e33c37b775d15
chris <chris@trivialsec.com>
2021-09-26 14:56:17 +1000

    more migration updates

--- src/routes/project.py
+++ src/routes/project.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from flask import render_template, Blueprint, abort
 from flask_login import current_user, login_required
 from trivialsec.helpers.config import config
-from trivialsec.models.domain import Domains
+from trivialsec.models.domain import Domain, DomainMonitoring
 from trivialsec.models.job_run import JobRuns
 from trivialsec.models.project import Project
 from trivialsec.models.finding import Findings
@@ -37,19 +36,17 @@ def page_project(canonical_id :str, page: int = 1):
     page = int(page)
     page_num = max(1, page)
     offset = max(0, page-1) * page_size
-    # search_filter = [
-    #     ('account_id', current_user.account_id),
-    #     ('project_id', project.project_id),
-    #     ('deleted', 0),
-    #     ('parent_domain_id', None),
-    # ]
-    # params['pagination'] = Domains().pagination(
-    #     search_filter=search_filter,
-    #     page_size=page_size,
-    #     page_num=page_num
-    # )
-    # params['pagination']['page_id'] = project.canonical_id
-    # params['pagination']['sub_page'] = 'domains'
+    search_filter = [
+        ('account_id', current_user.account_id),
+        ('project_id', project.project_id),
+    ]
+    params['pagination'] = DomainMonitoring().pagination(
+        search_filter=search_filter,
+        page_size=page_size,
+        page_num=page_num
+    )
+    params['pagination']['page_id'] = project.canonical_id
+    params['pagination']['sub_page'] = 'domains'
 
     project_dict = {'domains': []}
     project_dict['findings_info'] = Findings().count_informational([
@@ -89,33 +86,23 @@ def page_project(canonical_id :str, page: int = 1):
     for col in project.cols():
         project_dict[col] = getattr(project, col)
 
-    # domains = Domains()
-    # for domain in domains.find_by(search_filter, limit=page_size, offset=offset):
-    #     domain.get_stats()
-    #     domain_dict = {}
-    #     for col in domain.cols():
-    #         domain_dict[col] = getattr(domain, col)
-    #     domain_dict['thumbnail_url'] = f'https://{config.aws.get("public_bucket")}.s3-{config.aws.get("region_name")}.amazonaws.com/captures/{domain.name}-render-320x240.jpeg' if domain.screenshot else None
-    #     domain_dict['screen_url'] = f'https://{config.aws.get("public_bucket")}.s3-{config.aws.get("region_name")}.amazonaws.com/captures/{domain.name}-full.jpeg' if domain.screenshot else None
-    #     for domain_stat in domain.stats:
-    #         if domain_stat.domain_stat == DomainStat.HTTP_LAST_CHECKED:
-    #             domain_dict[DomainStat.HTTP_LAST_CHECKED] = domain_stat.domain_value
-    #             break
-    #     if domain_dict.get(DomainStat.HTTP_LAST_CHECKED):
-    #         http_last_checked = datetime.fromisoformat(domain_dict[DomainStat.HTTP_LAST_CHECKED]).replace(microsecond=0)
-    #         for domain_stat in domain.stats:
-    #             created_at = datetime.fromisoformat(domain_stat.created_at)
-    #             if created_at == http_last_checked:
-    #                 domain_dict[domain_stat.domain_stat] = {
-    #                     'value': domain_stat.domain_value,
-    #                     'data': domain_stat.domain_data,
-    #                 }
-    #     project_dict['domains'].append(domain_dict)
+    for domain_record in DomainMonitoring().find_by(search_filter, limit=page_size, offset=offset):
+        domain = Domain()
+        domain.hydrate(query_string=f'domain_name:"{domain_record.domain_name}"')
+        domain_dict = domain.get_doc()
+        domain_dict['domain_monitoring_id'] = domain_record.domain_monitoring_id
+        domain_dict['schedule'] = domain_record.schedule
+        domain_dict['enabled'] = domain_record.enabled
+        domain_dict['account_id'] = domain_record.account_id
+        domain_dict['project_id'] = domain_record.project_id
+        # domain_dict['thumbnail_url'] = f'https://{config.aws.get("public_bucket")}.s3-{config.aws.get("region_name")}.amazonaws.com/captures/{domain.domain_name}-render-320x240.jpeg' if domain.screenshot else None
+        # domain_dict['screen_url'] = f'https://{config.aws.get("public_bucket")}.s3-{config.aws.get("region_name")}.amazonaws.com/captures/{domain.domain_name}-full.jpeg' if domain.screenshot else None
+        project_dict['domains'].append(domain_dict)
 
     params['project'] = project_dict
     params['page_title'] = project.name
     params['toasts'] = []
-    if len(params['project']['domains']) == 1 and params['project']['domains'][0].get('http_last_checked') is None:
+    if len(params['project']['domains']) == 1:
         params['toasts'].append({
             'type': 'warning',
             'heading': 'Loading',
